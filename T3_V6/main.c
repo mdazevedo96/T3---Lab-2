700b05c465abc444fdf49e2d3852273f4a164395
HLKellermann <helena.kellermann@acad.ufsm.br>
2024-11-09 10:46:22 -0300

    Create main.c

---
+++ T3_V6/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+/*HEADERS DO PROGRAMA*/
+#include "aluno.h"
+#include "projeto.h"
+#include "professor.h"
+#include "vinculo.h"
+#include "menu.h"
+
+int main()
+{
+    menu();
+    return 0;
+}
